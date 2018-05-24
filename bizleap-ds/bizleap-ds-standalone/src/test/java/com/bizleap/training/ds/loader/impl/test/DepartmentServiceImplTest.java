20d8ec6e63128da9b293d71f84f2528e87ac4cad
BizleapIntern <bizleapinternship@gmail.com>
2018-05-24 12:02:14 +0630

    dsworkspace2

---
+++ bizleap-ds/bizleap-ds-standalone/src/test/java/com/bizleap/training/ds/loader/impl/test/DepartmentServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.bizleap.training.ds.loader.impl.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.bizleap.commons.domain.exception.ServiceUnavailableException;
+import com.bizleap.training.ds.service.DepartmentService;
+
+public class DepartmentServiceImplTest extends ServiceTest {
+	
+	@Autowired
+	@Qualifier("departmentService")
+	private DepartmentService departmentService;
+
+	static Logger logger = Logger.getLogger(DepartmentServiceImplTest.class);
+
+	//@Ignore
+	@Test
+	public void testFindByDepartmentBoId() throws ServiceUnavailableException {
+
+		assertNotNull(departmentService.findByDepartmentBoId("DEPT03"));
+		//logger.info("Department list: " + departmentService.findByDepartmentBoId("DEPT03"));
+		assertTrue(CollectionUtils.isNotEmpty(departmentService.findByDepartmentBoId("DEPT03")));
+	}
+
+	//@Ignore
+	@Test
+	public void testGetAllDepartment() throws IOException, ServiceUnavailableException {
+		assertEquals(6, departmentService.getAllDepartment().size());
+		//logger.info("Department Count : "+departmentService.getAllDepartment().size());
+	}
+}
